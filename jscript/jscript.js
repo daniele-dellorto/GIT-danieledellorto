3bfed8bcf9f070409d060b89a8ee71179827d15a
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2018-06-29 21:25:44 +0200

    access to gallery

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,1 +1,19 @@
+function col(){
+  $("#pic1").addClass("bianco")
+  $("#immagine1").addClass("scomp")}
 
+function bn(){
+  $("#pic2").addClass("bianco")
+  $("#immagine2").addClass("scomp")}
+
+function colneg(){
+  $("#pic1").removeClass("bianco")
+  $("#immagine1").removeClass("scomp")}
+
+function bnneg(){
+  $("#pic2").removeClass("bianco")
+  $("#immagine2").removeClass("scomp")}
+
+$("#pic1").hover(bn,bnneg)
+
+$("#pic2").hover(col,colneg)
