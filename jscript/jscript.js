149346bf9a305c2b7774de33dfc101d338fdf280
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2018-06-30 10:02:24 +0200

    gallery e contacts

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,29 +1,35 @@
 function col(){
   $("#pic1").addClass("bianco")
   $("#immagine1").addClass("scomp")
-  $("#title2").removeClass("invisible")}
+  $(".title2").removeClass("invisible")}
 
 function bn(){
   $("#pic2").addClass("bianco")
   $("#immagine2").addClass("scomp")
-  $("#title1").removeClass("invisible")}
+  $(".title1").removeClass("invisible")}
 
 function colneg(){
   $("#pic1").removeClass("bianco")
   $("#immagine1").removeClass("scomp")
-  $("#title2").addClass("invisible")}
+  $(".title2").addClass("invisible")}
 
 function bnneg(){
   $("#pic2").removeClass("bianco")
   $("#immagine2").removeClass("scomp")
-  $("#title1").addClass("invisible")}
+  $(".title1").addClass("invisible")}
 
 $("#pic1").hover(bn,bnneg)
 
 $("#pic2").hover(col,colneg)
 
 function contacts(){
-  $(".contacts").removeClass("invisible")
+  $(".insta").addClass("insta1")
+  $(".insta").removeClass("insta")
 }
 
-$(".contatti").click(contacts)
+function contactsneg(){
+  $(".insta1").addClass("insta")
+  $(".insta1").removeClass("insta1")
+}
+
+$(".hov").hover(contacts,contactsneg)
