b9045fcf25ad57ca6ecdf1941a1249dc1bcd1e3c
daniele-dellorto <37075250+daniele-dellorto@users.noreply.github.com>
2018-07-01 19:17:00 +0200

    jscr

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,22 +1,26 @@
 function col(){
   $("#pic1").addClass("bianco")
   $("#immagine1").addClass("scomp")
-  $(".title2").removeClass("invisible")}
+  $(".title2").removeClass("invisible")
+  $("#qu2").addClass("appearing")}
 
 function bn(){
   $("#pic2").addClass("bianco")
   $("#immagine2").addClass("scomp")
-  $(".title1").removeClass("invisible")}
+  $(".title1").removeClass("invisible")
+  $("#qu1").addClass("appearing")}
 
 function colneg(){
   $("#pic1").removeClass("bianco")
   $("#immagine1").removeClass("scomp")
-  $(".title2").addClass("invisible")}
+  $(".title2").addClass("invisible")
+  $("#qu2").removeClass("appearing")}
 
 function bnneg(){
   $("#pic2").removeClass("bianco")
   $("#immagine2").removeClass("scomp")
-  $(".title1").addClass("invisible")}
+  $(".title1").addClass("invisible")
+  $("#qu1").removeClass("appearing")}
 
 $("#immagine1").hover(bn,bnneg)
 
@@ -42,7 +46,7 @@ function animateintro(){
 $("#transizione1").hover(animateintro)
 
 function delay (URL) {
-    setTimeout( function() { window.location = URL }, 700 );
+    setTimeout( function() { window.location = URL }, 1000 );
 }
 
 function about(){
